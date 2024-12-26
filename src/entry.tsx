568591e364e35a0e03792121add644810ad411d5
Freewind <nowindlee@gmail.com>
2024-12-26 22:49:45 +0800

    migrate to vite

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
-import {Hello} from './hello'
+import { Hello } from './hello'
 
-ReactDOM.render(
-  <Hello/>,
-  document.body
+const root = createRoot(document.getElementById('root')!)
+root.render(
+  <React.StrictMode>
+    <Hello />
+  </React.StrictMode>
 )
